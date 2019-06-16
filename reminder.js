7fb51625a51b5cd06fcb0f695f694cc1cf0023c0
Marco Parrone <marco.parrone@gmail.com>
2019-06-16 16:47:45 +0200

    .

--- reminder.js
+++ reminder.js
@@ -2,36 +2,22 @@ var mySchedule = null;
 var myTitle = 'Reminder';
 var myBody = 'Reminder';
 
-function isNewNotificationSupported() {
-    if (!window.Notification || !Notification.requestPermission)
-        return false;
-    if (Notification.permission == 'granted') {
-        throw new Error('You must only call this before calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
-    }
-    try {
-        new Notification('');
-    } catch (e) {
-        if (e.name == 'TypeError')
-            return false;
-    }
-    return true;
-}
+navigator.serviceWorker.register('service-worker.js');
 
 function notifyMe() {
-    if (window.Notification && Notification.permission == 'granted') {
-	navigator.serviceWorker.getRegistrations().then(function(registrations) {
-	    registrations[0].showNotification(myTitle, {
+    try {
+	navigator.serviceWorker.getRegistration()
+	    .then(reg => reg.showNotification(myTitle, {
 		body: myBody,
 		icon: 'reminder-256.png',
 		tag: 'looping-reminder'
-	    });
-	});
-    } else if (isNewNotificationSupported()) {
-	Notification.requestPermission();
+	    }))
+	    .catch(err => alert('Service Worker registration error: ' + err));
+    } catch (err) {
+	alert('Notification API error: ' + err);
     }
 }
 
-navigator.serviceWorker.register('service-worker.js');
 function changeInterval() {
     var interval =  document.getElementById("intervalInput").value;
     document.getElementById("intervalText").innerHTML = interval;
@@ -40,9 +26,5 @@ function changeInterval() {
     if (mySchedule != null) {
 	clearInterval(mySchedule);
     }
-    if (window.Notification && Notification.permission == 'granted') {
-	mySchedule = setInterval(notifyMe, interval * 1000);
-    } else if (isNewNotificationSupported()) {
-	Notification.requestPermission();
-    }
+    mySchedule = setInterval(notifyMe, interval * 1000);
 }

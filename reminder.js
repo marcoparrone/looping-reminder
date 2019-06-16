012f4751fba222c4195ee1ccab33a65840b552f2
Marco Parrone <marco.parrone@gmail.com>
2019-06-16 15:59:08 +0200

    New attempt at chrome on android compatibility.

--- reminder.js
+++ reminder.js
@@ -5,10 +5,9 @@ var myBody = 'Reminder';
 function isNewNotificationSupported() {
     if (!window.Notification || !Notification.requestPermission)
         return false;
-    if (Notification.permission == 'granted')
-        throw new Error('You must only call this \*before\* calling
-Notification.requestPermission(), otherwise this feature detect would bug the
-user with an actual notification!');
+    if (Notification.permission == 'granted') {
+        throw new Error('You must only call this before calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
+    }
     try {
         new Notification('');
     } catch (e) {
@@ -21,24 +20,28 @@ user with an actual notification!');
 function notifyMe() {
     if (window.Notification && Notification.permission == 'granted') {
 	navigator.serviceWorker.getRegistrations().then(function(registrations) {
-		registrations[0].showNotification(myTitle, {
-		    body: myBody,
-		    icon: 'reminder-256.png',
-		    tag: 'looping-reminder'
-		});
+	    registrations[0].showNotification(myTitle, {
+		body: myBody,
+		icon: 'reminder-256.png',
+		tag: 'looping-reminder'
 	    });
+	});
     } else if (isNewNotificationSupported()) {
 	Notification.requestPermission();
     }
 }
 
 function changeInterval() {
-  var interval =  document.getElementById("intervalInput").value;
-  document.getElementById("intervalText").innerHTML = interval;
-  myTitle = document.getElementById("titleInput").value;
-  myBody = document.getElementById("bodyInput").value;
-  if (mySchedule != null) {
-    clearInterval(mySchedule);
-  }
-  mySchedule = setInterval(notifyMe, interval * 1000);
+    var interval =  document.getElementById("intervalInput").value;
+    document.getElementById("intervalText").innerHTML = interval;
+    myTitle = document.getElementById("titleInput").value;
+    myBody = document.getElementById("bodyInput").value;
+    if (mySchedule != null) {
+	clearInterval(mySchedule);
+    }
+    if (window.Notification && Notification.permission == 'granted') {
+	mySchedule = setInterval(notifyMe, interval * 1000);
+    } else if (isNewNotificationSupported()) {
+	Notification.requestPermission();
+    }
 }

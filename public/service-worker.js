1de0d0f9c833f223a6d8cfecd439d2ac854e0a65
Marco Parrone <marco@marcoparrone.com>
2020-12-14 12:00:59 +0100

    fixed caching.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded`);
-} else {
-  console.log(`Boo! Workbox didn't load`);
-}
-
-// Cache text files but update in the background.
+// Cache site resources but update in the background.
 workbox.routing.registerRoute(
-    /\.(?:css|html|json)$/, 
-    new workbox.strategies.StaleWhileRevalidate({
-	cacheName: 'text-cache',
-    })
-);
-
-// Cache image files - maximum 20 images for a maximum age of 30 days.
-workbox.routing.registerRoute(
-  /\.(?:png|jpg|jpeg|svg|gif)$/, 
-  new workbox.strategies.CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      })
-    ],
+  /.marcoparrone\.github\.io/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'mparrone',
   })
 );
+
 
 // Cache google resources but update in the background.
 workbox.routing.registerRoute(

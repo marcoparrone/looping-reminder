c71806653dbcd9bb400cedc20e69b261d52dff32
Marco Parrone <marco@marcoparrone.com>
2020-12-14 23:24:49 +0100

    Fixed caching.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 var m = document.createElement('meta');
 m.name = 'theme-color';
@@ -10,5 +9,3 @@ m.content = getComputedStyle(document.documentElement).getPropertyValue('--color
 document.head.appendChild(m);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.register();

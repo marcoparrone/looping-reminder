d97a723d92994bffe5dd77cc9b5635c2a0656989
Marco Parrone <marco@marcoparrone.com>
2021-02-28 10:49:33 +0100

    Fixed wrong timestamp.

--- src/timestamp.js
+++ src/timestamp.js
@@ -10,10 +10,10 @@ export default function get_timestamp(){
   let dt=new Date();
   let year=dt.getFullYear();
   let month=dt.getMonth();
-  let day=dt.getDay();
+  let day=dt.getDate();
   let hours=dt.getHours();
   let min=dt.getMinutes();
   let sec=dt.getSeconds();
 
- return year + '-' + pad(month) + '-' + pad(day) + 'T' + pad(hours) + '-' + pad(min) + '-' + pad(sec);
+ return year + pad(month + 1) + pad(day) + 'T' + pad(hours) + pad(min) + pad(sec);
 }
